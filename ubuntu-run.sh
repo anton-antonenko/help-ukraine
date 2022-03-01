c28353d9ea7d3c9dc890ee909137fb1810ff8052
Anton Antonenko <aantonenko@apple.com>
2022-03-01 14:46:54 +0200

    fixes

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -3,11 +3,12 @@
 apt-get update
 apt-get -y install git docker-ce docker build-essential
 
-git clone git@github.com:anton-antonenko/help-ukraine.git
+git clone https://github.com/anton-antonenko/help-ukraine.git
 cd help-ukraine
 ./goinstall.sh
+source /home/ubuntu/.bashrc
 make build
 
-chmod +x ./build/darwin-amd64/help-ukraine
+chmod +x ./build/linux-amd64/help-ukraine
 
-./build/darwin-amd64/help-ukraine
+./build/linux-amd64/help-ukraine
